553b6cd9deae3d7de01e8de595934cc1c3f2f0cf
Oliver Chou <oliver@mgxcopy.com>
2013-03-15 23:24:36 -0700

    fixed archive_user + added headers to all script files

--- Logger.py
+++ Logger.py
@@ -1,14 +1,14 @@
-from datetime import datetime
+import datetime
 
 class Logger:
 	log_file = ""
 
 	def print_info(self, script_name, result):
-		output_str = "%s %s %s %s" % (datetime.now(), "INFO", script_name, result) 
+		output_str = "%s %s %s %s" % (datetime.datetime.now(), "INFO", script_name, result) 
 		self.log(output_str)
 
 	def print_warn(self, script_name, result):
-		output_str = "%s %s %s %s" % (datetime.now(), "WARN", script_name, result) 
+		output_str = "%s %s %s %s" % (datetime.datetime.now(), "WARN", script_name, result) 
 		self.log(output_str)
 
 	def log(self, output):
